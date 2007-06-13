1100b9befd70d4ba7de5c43a33ee4d78c334aaa1
cafsim <cafsim@96f51ff7-9232-0410-a50d-8b69926bcfac>
2007-06-13 18:16:44 +0000

    fixed indentation.

--- src/org/gaplan/glanet/Blogs.java
+++ src/org/gaplan/glanet/Blogs.java
@@ -1,16 +1,9 @@
-/**
- * 
- */
 package org.gaplan.glanet;
 
 import java.util.ArrayList;
 
-/**
- * @author cafer
- *
- */
 public class Blogs {
-	public ArrayList<BlogItem> getAll() {
-		return FetcherServlet.getAllItems();
-	}
+    public ArrayList<BlogItem> getAll() {
+        return FetcherServlet.getAllItems();
+    }
 }
