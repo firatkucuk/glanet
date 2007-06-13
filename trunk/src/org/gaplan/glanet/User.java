12ce4d9ffe00afae2744c5d498dfac1b024a6e92
cafsim <cafsim@96f51ff7-9232-0410-a50d-8b69926bcfac>
2007-06-13 18:15:15 +0000

    * fixed indentation * tab to spaces

--- trunk/src/org/gaplan/glanet/User.java
+++ trunk/src/org/gaplan/glanet/User.java
@@ -1,80 +1,52 @@
-/**
- * 
- */
 package org.gaplan.glanet;
 
-/**
- * @author cafer
- *
- */
 public class User {
-	private String userName;
-	private String url;
-	private String fullName;
-	private String rssUrl;
-	private String email;
-        private String headUrl;
+    private String userName;
+    private String url;
+    private String fullName;
+    private String rssUrl;
+    private String email;
+    private String headUrl;
 
-	/**
-	 * @return the userName
-	 */
-	public String getUserName() {
-		return userName;
-	}
-	/**
-	 * @param userName the userName to set
-	 */
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	/**
-	 * @return the url
-	 */
-	public String getUrl() {
-		return url;
-	}
-	/**
-	 * @param url the url to set
-	 */
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	/**
-	 * @return the fullName
-	 */
-	public String getFullName() {
-		return fullName;
-	}
-	/**
-	 * @param fullName the fullName to set
-	 */
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-	/**
-	 * @return the rssUrl
-	 */
-	public String getRssUrl() {
-		return rssUrl;
-	}
-	/**
-	 * @param rssUrl the rssUrl to set
-	 */
-	public void setRssUrl(String rssUrl) {
-		this.rssUrl = rssUrl;
-	}
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRssUrl() {
+        return rssUrl;
+    }
+
+    public void setRssUrl(String rssUrl) {
+        this.rssUrl = rssUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getHeadUrl() {
         return headUrl;
@@ -83,5 +55,4 @@ public class User {
     public void setHeadUrl(String headUrl) {
         this.headUrl = headUrl;
     }
-
 }
