7e1e823f7a7a6b83258eea0069750ea17221a469
cafsim <cafsim@96f51ff7-9232-0410-a50d-8b69926bcfac>
2007-06-13 18:18:11 +0000

    fixed indentation.

--- trunk/src/org/gaplan/glanet/FetcherServlet.java
+++ trunk/src/org/gaplan/glanet/FetcherServlet.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.gaplan.glanet;
 
 import java.util.ArrayList;
@@ -9,47 +6,34 @@ import java.util.Timer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-/**
- * @author cafer
- *
- */
 public class FetcherServlet extends HttpServlet {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1807930740483845702L;
-	public static ArrayList<BlogItem> allItems;
-	private Timer timer;
-	private ContentGenerator cg;
-	
-	public void init() throws ServletException {
-		System.out.println("FetcherServlet init oluyor...");
-		allItems = new ArrayList<BlogItem>();
-		timer = new Timer();
-		cg = new ContentGenerator(allItems);
-		timer.scheduleAtFixedRate(cg, 20000, 5 * 60000);
-		System.out.println("FetcherServlet init oldu...");
-		cg.setRun(true);
-	}
-	
-	public void destroy() {
-		cg.setRun(false);
-		timer.cancel();
-		System.out.println("timer canceled");
-	}
+    private static final long serialVersionUID = 1807930740483845702L;
+    public static ArrayList<BlogItem> allItems;
+    private Timer timer;
+    private ContentGenerator cg;
 
-	/**
-	 * @return the allItems
-	 */
-	public static ArrayList<BlogItem> getAllItems() {
-		return allItems;
-	}
+    public void init() throws ServletException {
+        System.out.println("FetcherServlet init oluyor...");
+        allItems = new ArrayList<BlogItem>();
+        timer = new Timer();
+        cg = new ContentGenerator(allItems);
+        timer.scheduleAtFixedRate(cg, 20000, 5 * 60000);
+        System.out.println("FetcherServlet init oldu...");
+        cg.setRun(true);
+    }
 
-	/**
-	 * @param allItems the allItems to set
-	 */
-	public static void setAllItems(ArrayList<BlogItem> allItems) {
-		FetcherServlet.allItems = allItems;
-	}
+    public void destroy() {
+        cg.setRun(false);
+        timer.cancel();
+        System.out.println("timer canceled");
+    }
+
+    public static ArrayList<BlogItem> getAllItems() {
+        return allItems;
+    }
+
+    public static void setAllItems(ArrayList<BlogItem> allItems) {
+        FetcherServlet.allItems = allItems;
+    }
 }
